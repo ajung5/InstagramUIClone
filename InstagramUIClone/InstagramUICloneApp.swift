46528c8d7f99b18b6fdbe8a8386e5fcbcc809a9e
Agung Nawawi <agung@fairtech.com.sg>
2023-12-03 17:51:46 +0700

    Initial Commit

---
+++ InstagramUIClone/InstagramUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramUICloneApp.swift
+//  InstagramUIClone
+//
+//  Created by Agung Nawawi on 03/12/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
